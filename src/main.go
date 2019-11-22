b51c2ed0ebb904340fab97d1c0e85d2be31fcfd0
David Petter Alves <cfdavidpetter@gmail.com>
2019-11-22 09:49:12 -0300

    first commit

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl, _ := template.ParseFiles("templates/index.html")
+	data := map[string]string{
+		"Title": "Go Store :)",
+	}
+	w.WriteHeader(http.StatusOK)
+	tpl.Execute(w, data)
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	fmt.Println("Server is up and listening on port 8080.")
+	http.ListenAndServe(":8080", nil)
+}
