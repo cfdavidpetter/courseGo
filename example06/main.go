a20e3f0bd0ff0e5bf43c27ddfb4072c636c8c8c4
David Petter Alves <cfdavidpetter@gmail.com>
2020-01-17 17:10:21 -0300

    novo example

---
+++ example06/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func main() {
+	fmt.Println("Begini script FTP.")
+	fmt.Println("")
+	time.Sleep(1000 * time.Millisecond)
+
+	var CEP string
+	fmt.Print("Enter your CEP: ")
+	fmt.Scanln(&CEP)
+
+	var url strings.Builder
+	url.WriteString("https://viacep.com.br/ws/")
+	url.WriteString(CEP)
+	url.WriteString("/json/")
+	fmt.Println("Execing GET of", url.String())
+	fmt.Println("")
+
+	response, err := http.Get(url.String())
+	if err != nil { //Se tiver error na request.
+		log.Fatalln(err)
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil { //Se tiver error no body.
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(body))
+	time.Sleep(10000 * time.Millisecond)
+}
